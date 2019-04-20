71e0fe6539303152d884d326e89b7218a371e103
roma <romhedoo@gmail.com>
2019-04-20 13:00:44 +0200

    Added 3. version

--- bubble-sort.py
+++ bubble-sort.py
@@ -27,3 +27,25 @@ bubble_sort_unoptimized(nums)
 bubble_sort(nums1)
 print("POST SORT: {0}".format(nums))
 print(nums1)
+
+
+#------Alternative ----------
+
+def bubble_sort(arr): 
+    def swap(i, j):
+        arr[i], arr[j] = arr[j], arr[i]
+
+    n = len(arr)
+    swapped = True
+    
+    x = -1 #init index
+    while swapped: #need to control if all elements are sorted
+        swapped = False #setting ot False all the time we inspect next element
+        x = x + 1 #next element / next 'big' iteration (after each iteration 1 element went to the end of array (=sorted))
+        for i in range(1, n-x): # for indexes 1 until end (len-sorted elements)
+            if arr[i - 1] > arr[i]: # arr[0] > arr[1] and so on
+                arr[i-1], arr[j] = arr[j], arr[i-1]
+                swapped = True
+            #else: pass to the swapped = False
+                    
+    return arr
